23e5435774dfb67820a7096679181159b3ab61f9
Carlos Veeck <caiqueveeck@gmail.com>
2024-06-09 18:36:49 -0400

    versão final do exercício com os códigos finalizados, análise de desempenho feita e slides de apresentação

--- GRPC/Server/webserver.go
+++ GRPC/Server/webserver.go
@@ -1,28 +1,31 @@
 package main
+
 import (
-    "log"
-    "net"
-    "net/http"
-    "net/rpc"
-    "time"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"time"
 )
+
 type Args struct{}
 type TimeServer string
+
 func (t *TimeServer) GiveServerTime(args *Args, reply *string) error {
-    // Fill reply pointer to send the data back
-    *reply = time.Now().Format(time.RFC1123)
-    return nil
+
+	*reply = time.Now().Format(time.RFC1123)
+	return nil
 }
 func main() {
-    // Create a new RPC server
-    timeserver := new(TimeServer)
-    // Register RPC server
-    rpc.Register(timeserver)
-    rpc.HandleHTTP()
-    // Listen for requests on port 1234
-    l, e := net.Listen("tcp", ":2233")
-    if e != nil {
-        log.Fatal("listen error:", e)
-    }
-    http.Serve(l, nil)
+
+	timeserver := new(TimeServer)
+
+	rpc.Register(timeserver)
+	rpc.HandleHTTP()
+
+	l, e := net.Listen("tcp", ":2233")
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	http.Serve(l, nil)
 }
--- Sockets/Client/client.go
+++ Sockets/Client/client.go
@@ -5,15 +5,22 @@ import (
     "io"
     "log"
     "net/http"
+    "time"
 )
 
 func main() {
+    // Marca o início do tempo de requisição
+    start := time.Now()
+
     // Faz uma requisição GET para o servidor
-	resp, err := http.Get("http://localhost:5555/time")
+    resp, err := http.Get("http://localhost:5555/time")
     if err != nil {
         log.Fatalf("Erro ao fazer a requisição: %v", err)
     }
     defer resp.Body.Close()
+
+    // Marca o fim do tempo de requisição
+    elapsed := time.Since(start)
 
     // Lê o corpo da resposta
     body, err := io.ReadAll(resp.Body)
@@ -23,4 +30,5 @@ func main() {
 
     // Imprime a resposta
     fmt.Printf("Resposta do servidor: %s\n", body)
+    fmt.Printf("Tempo de resposta: %s\n", elapsed)
 }
